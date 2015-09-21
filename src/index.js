540cf4a693b25097d9040ec5cfa5b81fe10ed7c3
Steven Nguyen <steven@culturelabs.la>
2015-09-20 17:57:15 -0700

    forgot to save

--- src/index.js
+++ src/index.js
@@ -1,59 +1,53 @@
+import crypto from 'crypto';
+import _omit from 'lodash/object/omit';
+
 export default class ShopifyAPI {
   constructor(config) {
     this.config = config;
-    this.baseUrl = 'https://' + this.config.domain + '/admin';
+    this.baseUrl = 'https://' + this.config.domain;
+    this.reqHeaders = {
+      'accept': 'application/json',
+      'content-type': 'application/json',
+      'x-shopify-access-token': this.config.token
+    }
   }
 
   get(resource, queryObj) {
-    let reqSettings = {
-      method: 'GET',
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'x-shopify-access-token': this.config.token
-      }
-    }
+    let reqSettings = { method: 'GET', this.reqHeaders }
     let query = querystring.stringify(queryObj);
 
     return fetch(this.baseUrl + resource + query, { reqSettings });
   }
   post(resource) {
-    let reqSettings = {
-      method: 'POST'
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'x-shopify-access-token': this.config.token
-      }
-    }
+    let reqSettings = { method: 'POST', this.reqHeaders }
 
     return fetch(this.baseUrl + resource, { reqSettings });
   }
   put(resource) {
-    let reqSettings = {
-      method: 'PUT',
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'x-shopify-access-token': this.config.token
-      }
-    };
+    let reqSettings = { method: 'PUT', this.reqHeaders };
 
-    return fetch (this.baseUrl + resource, { reqSettings });
+    return fetch(this.baseUrl + resource, { reqSettings });
   }
   delete(resource) {
-    let reqSettings = {
-      method: 'DELETE',
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/json',
-        'x-shopify-access-token': this.config.token
-      }
-    }
+    let reqSettings = { method: 'DELETE', this.reqHeaders }
+
+    return fetch(this.baseUrl + resource, { reqSettings });
   }
+  validateSignature(query) {
+    let queryObj = _omit(query, ['hmac', 'signature']);
+    let queryStr = querystring.stringify(queryObj);
 
+    let digest = crypto.createHmac('sha256', this.config.secret)
+      .update(queryStr)
+      .digest('hex');
 
-  static validateSignature(query) {}
-  static verifyWebhook(query) {}
+    return query.hmac === digest;
+  }
+  verifyWebhook(body, hmac) {
+    let digest = crypto.createHmac('sha256', this.config.secret)
+      .update(body)
+      .digest('base64')
 
+    return hmac === digest;
+  }
 }
