913d39658c74bf1c96991f0cf1110bf26cb184ae
Steven Nguyen <steven@culturelabs.la>
2015-10-22 01:23:46 -0700

    fix linting and add app proxy signature validation

--- src/index.js
+++ src/index.js
@@ -1,38 +1,32 @@
-import crypto from 'crypto';
-import _omit from 'lodash/object/omit';
-import fetch from 'node-fetch';
-
-const defaultConfig = {
-  domain: '',
-  scope: '',
-  key: '',
-  secret: '',
-}
+import crypto from "crypto";
+import _ from "lodash";
+import fetch from "node-fetch";
+import querystring from "querystring";
 
 export default class ShopifyAPI {
 
   constructor(config) {
-    this.config = config;
-    this.baseUrl = 'https://' + this.config.domain;
+   this.config = config;
+    this.baseUrl = "https://" + this.config.domain;
     this.reqHeaders = {
-      'accept': 'application/json',
-      'content-type': 'application/json',
-      'x-shopify-access-token': this.config.accessToken || ''
-    }
+      "accept": "application/json",
+      "content-type": "application/json",
+      "x-shopify-access-token": this.config.accessToken || ""
+    };
   }
 
   get(resource, queryObj = {}) {
     let query = querystring.stringify(queryObj);
 
-    return fetch(this.baseUrl + resource + query, { 
-      method: 'GET',
+    return fetch(this.baseUrl + resource + query, {
+      method: "GET",
       headers: this.reqHeaders
     });
   }
 
   post(resource, body = {}) {
     return fetch(this.baseUrl + resource, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(body),
       headers: this.reqHeaders
     });
@@ -40,7 +34,7 @@ export default class ShopifyAPI {
 
   put(resource, body = {}) {
     return fetch(this.baseUrl + resource, {
-      method: 'PUT',
+      method: "PUT",
       body: JSON.stringify(body),
       headers: this.reqHeaders
     });
@@ -48,38 +42,54 @@ export default class ShopifyAPI {
 
   delete(resource) {
     return fetch(this.baseUrl + resource, {
-      method: 'DELETE',
+      method: "DELETE",
       headers: this.reqHeaders
     });
   }
 
   authURL() {
     let queryObj = {
-      client_id: this.config.key,
+      clientID: this.config.key,
       scope: this.config.scope,
-      redirect_uri: this.config.redirectURI
-    }
+      redirectURI: this.config.redirectURI
+    };
     let queryStr = querystring.stringify(queryObj);
     return `${this.baseURL}/admin/oauth/authorize?${queryStr}`;
   }
 
   validateSignature(query) {
     let queryStr = querystring.stringify(
-      _omit(query, ['hmac', 'signature']
+      _.omit(query, ["hmac", "signature"])
     );
-    let digest = crypto.createHmac('sha256', this.config.secret)
+    let digest = crypto.createHmac("sha256", this.config.secret)
       .update(queryStr)
-      .digest('hex');
+      .digest("hex");
 
     return query.hmac === digest;
   }
 
   verifyWebhook(body, hmac) {
-    let digest = crypto.createHmac('sha256', this.config.secret)
+    let digest = crypto.createHmac("sha256", this.config.secret)
     .update(body)
-    .digest('base64')
+    .digest("base64");
 
     return hmac === digest;
+  }
+
+  verifyProxySignature(query) {
+    let sortedParams = _.chain(query)
+      .omit(["signature"])
+      .pairs()
+      .sort()
+      .map(item => { return item.join("="); })
+      .join("")
+      .value();
+
+    let calculatedSignature = crypto.createHmac("sha256", this.config.secret)
+      .update(sortedParams)
+      .digest("hex");
+
+    return calculatedSignature === query.signature;
   }
 
 }
